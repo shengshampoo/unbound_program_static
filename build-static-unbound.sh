4861600c17c4d8ffc77b1f213f91f69fc01e2258
shengshampoo <shengzh@tutanota.com>
2025-09-25 10:58:17 +0000

    Create build-static-unbound.sh

---
+++ build-static-unbound.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+
+set -e
+
+WORKSPACE=/tmp/workspace
+mkdir -p $WORKSPACE
+mkdir -p /work/artifact
+
+# brotli
+cd $WORKSPACE
+git clone https://github.com/google/brotli.git
+cd brotli
+mkdir build
+cd build
+cmake -G Ninja -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=MinSizeRel -DBUILD_SHARED_LIBS=OFF ..
+ninja
+ninja install
+
+# libev
+cd $WORKSPACE
+curl -sL http://dist.schmorp.de/libev/libev-4.33.tar.gz | tar x --gzip
+cd libev-4.33
+LDFLAGS="-static --static -no-pie -s" ./configure --prefix=/usr
+make
+make install
+
+# nghttp3
+cd $WORKSPACE
+git clone https://github.com/ngtcp2/nghttp3.git
+cd nghttp3
+git submodule update --init --recursive
+autoreconf -i
+LDFLAGS="-static --static -no-pie -s" ./configure --prefix=/usr --enable-lib-only
+make
+make install
+
+# ngtcp2
+cd $WORKSPACE
+git clone https://github.com/ngtcp2/ngtcp2.git
+cd ngtcp2
+git submodule update --init --recursive
+autoreconf -i
+LDFLAGS="-static --static -no-pie -s" ./configure --prefix=/usr --enable-lib-only --with-libbrotlienc --with-libbrotlidec
+make
+make install
+
+# unbound
+cd $WORKSPACE
+curl -s https://www.nlnetlabs.nl/downloads/unbound/unbound-1.24.0.tar.gz | tar x --gzip
+cd unbound-1.24.0
+LDFLAGS="-static --static -no-pie -s  -lngtcp2_crypto_ossl -lnghttp3 -lnghttp2 -lcrypto -lssl -lc" \
+ ./configure --with-libevent --with-libexpat --with-ssl --enable-dnscrypt --enable-ipset \
+ --enable-dnstap --enable-dnscrypt --with-libmnl --enable-subnet --prefix=/usr/local/unboundmm \
+ --with-libnghttp2 --with-libngtcp2
+ make
+ make install
+
+cd /usr/local
+tar vcJf ./unboundmm.tar.xz unboundmm
+
+mv ./unboundmm.tar.xz /work/artifact/
